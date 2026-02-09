6da29172855023ad9ba599e9eefeb8e47a093861
zamcedrickdapug-hash <zamcedrickdapug@gmail.com>
2026-02-09 16:31:39 +0800

    Create app_colors.dart with color constants

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+// Color Constants for the Application
+
+class AppColors {
+  static const int primaryColor = 0xFF6200EE;
+  static const int secondaryColor = 0xFF03DAC5;
+  static const int backgroundColor = 0xFFFFFFFF;
+  static const int textColor = 0xFF000000;
+  static const int errorColor = 0xFFB00020;
+}
