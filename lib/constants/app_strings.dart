e0d52099c1d7f12c4c30228451d28657ca4439d8
zamcedrickdapug-hash <zamcedrickdapug@gmail.com>
2026-02-09 16:31:48 +0800

    Create lib/constants/app_strings.dart with string constants and messages

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,8 @@
+// This file contains string constants and messages used in the application
+
+class AppStrings {
+    static const String welcomeMessage = 'Welcome to the app!';
+    static const String errorMessage = 'An error occurred, please try again.';
+    static const String loadingMessage = 'Loading...';
+    // Add more string constants as needed
+}
